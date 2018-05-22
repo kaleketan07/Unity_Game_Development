c624c8195a7b65a9e6e19b0842f8882b4b099a82
Ketan Kale <kaleketan07@gmail.com>
2018-05-22 01:49:05 -0400

    intital commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    public Vector3 offset;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () { // runs every frame but is guaranteed to run after all updates have been processed
+        transform.position = player.transform.position + offset;	
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+   
+
+    void Start() {
+
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";    
+    }
+
+    void FixedUpdate() {
+
+        float veritcalMovement = Input.GetAxis("Vertical");
+        float horizontalMovement = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(horizontalMovement, 0, veritcalMovement);
+        rb.AddForce(movement * speed);
+
+
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("pickup")) {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText() {
+
+        if (count == 10) {
+            winText.text = "You win!";
+        }
+        countText.text = "Count: " + count.ToString();
+    }
+
+}
